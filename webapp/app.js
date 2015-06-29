1bfc2a0006c33f06cc6248c1a55215b432ced9e9
Matthew Dyer <matthew.dyer@aura.org.nz>
2015-06-29 12:55:10 +1200

    updated schema to enforce required constraints

--- webapp/app.js
+++ webapp/app.js
@@ -15,59 +15,118 @@ eventApp.controller('EventController', function ($scope, $http) {
   var validator = require('is-my-json-valid');
 
 
-  var schema = {
+  var schema =  {
     "$schema": "http://json-schema.org/draft-04/schema#",
     "definitions": {
-      "contactDetail": {
-        "type": "object",
-        "properties": {
-          "type": {"type": "string"},
-          "value": {"type": "string"}
-        },
-      },
-      "person": {
-        "type": "object",
-        "properties": {
-          "name": {"type": "string"},
-          "role": {"type": "string"},
-          "contactDetails": {"type": "array", "items": {"$ref": "#/definitions/contactDetail"}}
-        },
-      },
-      "event": {
-        "type": "object",
-        "properties": {
-          "title": {"type": "string"},
-          "location": {"type": "string"},
-          "startTime": {"type": "string"},
-          "endTime": {"type": "string"}
-        },
-      },
-      "subEvent": {
-        "type": "object",
-        "allOf": [
-          {"$ref": "#/definitions/event"},
-          {
+        "contactDetail": {
+            "type": "object",
             "properties": {
-              "description": {"type": "string"},
-              "type": {"type": "string"},
-              "people": {"type": "array", "items": {"$ref": "#/definitions/person"}}
+                "type": {
+                    "type": "string"
+                },
+                "value": {
+                    "type": "string"
+                }
             },
-          }
-        ]
-      }
+            "required": [
+                "type",
+                "value"
+            ]
+        },
+        "person": {
+            "type": "object",
+            "properties": {
+                "name": {
+                    "type": "string"
+                },
+                "role": {
+                    "type": "string"
+                },
+                "contactDetails": {
+                    "type": "array",
+                    "items": {
+                        "oneof": [
+                            {
+                                "$ref": "#/definitions/contactDetail"
+                            }
+                        ]
+                    }
+                }
+            }
+        },
+        "event": {
+            "type": "object",
+            "properties": {
+                "title": {
+                    "type": "string"
+                },
+                "location": {
+                    "type": "string"
+                },
+                "startTime": {
+                    "type": "string"
+                },
+                "endTime": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "title","location"
+            ]
+        },
+        "subEvent": {
+            "type": "object",
+            "allOf": [
+                {
+                    "$ref": "#/definitions/event"
+                },
+                {
+                    "properties": {
+                        "description": {
+                            "type": "string"
+                        },
+                        "type": {
+                            "type": "string"
+                        },
+                        "people": {
+                            "type": "array",
+                            "items": {
+                                "$ref": "#/definitions/person"
+                            }
+                        }
+                    }
+                }
+            ]
+        }
     },
     "title": "Conference",
     "type": "object",
-    "allOf": [
-      {"$ref": "#/definitions/event"},
-      {
-        "properties": {
-          "organiser": {"type": "string"},
-          "subEvents": {"type": "array", "items": {"$ref": "#/definitions/subEvent"}}
+    "properties": {
+        "event": {
+            "allOf": [
+                {
+                    "$ref": "#/definitions/event"
+                },
+                {
+                    "properties": {
+                        "organiser": {
+                            "type": "string"
+                        },
+                        "subEvents": {
+                            "type": "array",
+                            "items": {
+                                "$ref": "#/definitions/subEvent"
+                            }
+                        }
+                    },"required":["subEvents"]
+                }
+            ]
         }
-      }
+    },
+    "required": [
+        "event"
     ]
-  };
+};
 
     var validate = validator(schema, {
         verbose: true,

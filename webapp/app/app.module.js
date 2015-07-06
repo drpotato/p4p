3f1cb7dcb75db14a4b3631d9f410d266ab335c1f
Chris Morgan <chris.j.r.morgan@gmail.com>
2015-07-06 13:12:00 +1200

    fixed file upload (not sure why function was deleted) and reindented js

--- webapp/app/app.module.js
+++ webapp/app/app.module.js
@@ -12,30 +12,46 @@ eventApp.directive('customOnChange', function () {
 
 eventApp.controller('EventController', function ($scope, $http, openConferenceFormat) {
 
-    $scope.validate = function () {
-      var eventJSON = JSON.parse($scope.jsonText);
-      var result = openConferenceFormat.validate(eventJSON);
-      if (result.errors) {
-          //Handle Error States
-          $scope.event = null;
-          result.errors.forEach(function (error) {
-            alert(error.field + " " + error.message);
-          });
-        } else {
-          $scope.event = eventJSON.event;
-          console.log($scope.event);
-        }
-    };
+  $scope.uploadFile = function (evt) {
+    var files = evt.target.files; // FileList object
 
-    $scope.makeICS = function(){
-        //TODO: maybe this can be an angular module so we don't have to declare it in the html as well?
-        var cal = ics();
-        $scope.event.subEvents.forEach(function(subEvent){
-            console.log(subEvent);
-            cal.addEvent(subEvent.title,subEvent.title,subEvent.location,subEvent.startTime, subEvent.endTime);
-        });
-        cal.download();
+    if (files.length != 1) {
+      alert("Only 1 file");
+    }
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $scope.jsonText = e.target.result;
+      $scope.$apply();
     };
+    reader.readAsText(file);
+  };
+
+
+  $scope.validate = function () {
+    var eventJSON = JSON.parse($scope.jsonText);
+    var result = openConferenceFormat.validate(eventJSON);
+    if (result.errors) {
+      //Handle Error States
+      $scope.event = null;
+      result.errors.forEach(function (error) {
+        alert(error.field + " " + error.message);
+      });
+    } else {
+      $scope.event = eventJSON.event;
+      console.log($scope.event);
+    }
+  };
+
+  $scope.makeICS = function(){
+    //TODO: maybe this can be an angular module so we don't have to declare it in the html as well?
+    var cal = ics();
+    $scope.event.subEvents.forEach(function(subEvent){
+      console.log(subEvent);
+      cal.addEvent(subEvent.title,subEvent.title,subEvent.location,subEvent.startTime, subEvent.endTime);
+    });
+    cal.download();
+  };
 
 
 });

f421e571105c743c9d10ea4cc887e6b198578227
Matthew Dyer <matthew.dyer@aura.org.nz>
2015-07-09 13:15:15 +1200

    modals :D

--- webapp/app/components/subEvent/subEventDirective.js
+++ webapp/app/components/subEvent/subEventDirective.js
@@ -1,18 +1,35 @@
 'use strict';
 
-angular.module('esad.subEvent', [])
+angular.module('esad.subEvent', ['ui.bootstrap','ui.bootstrap.modal'])
 
-.controller('SubEventController', function ($scope, $element) {
+.controller('SubEventController', function ($scope, $modal) {
   $scope.expanded = false;
-  $scope.expand = function () {
-    $scope.expanded = !$scope.expanded;
+  $scope.expand = function (size) {
+    var modalInstance = $modal.open({
+      animation: true,
+      templateUrl: 'myModalContent.html',
+      controller: 'SubEventModalInstanceCtrl',
+      size: size,
+      resolve: {
+        subEvent: function () {
+          return $scope.subEvent;
+        }
+      }
+    });
+  };
+}).controller('SubEventModalInstanceCtrl', function ($scope, $modalInstance, subEvent) {
+  $scope.subEvent = subEvent;
+  $scope.ok = function () {
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
   };
 })
-
 .directive('subEvent', function () {
   return {
     restrict: 'E',
-    transclude: true,
     scope: {
       subEvent: "=subEvent"
     },
